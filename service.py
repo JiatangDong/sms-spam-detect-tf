4b3ae9d2b57afa7bdb6b525d7cc6b6353109fc8b
Jiatang Dong <jiadon@microsoft.com>
2022-06-20 15:36:51 -0700

    init project

---
+++ service.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+import tensorflow as tf
+import tensorflow_text as text
+
+app = Flask(__name__)
+model = tf.keras.models.load_model('SMSSpamModel')
+
+@app.route('/check_spam', methods = ['POST'])
+def check_spam():
+    predict = model.predict(request.get_json())
+    return jsonify(predict.flatten().tolist())
+
+if __name__ == '__main__':
+    app.run(debug = True)
